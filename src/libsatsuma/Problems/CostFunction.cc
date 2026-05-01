df1d73b1f375d3e4c3ac32705940e81b6f38f920
Martin Heistermann <git@mheistermann.de>
2026-05-01 16:05:33 +0200

    Fix bug in find_optimal_integer.

--- src/libsatsuma/Problems/CostFunction.cc
+++ src/libsatsuma/Problems/CostFunction.cc
@@ -1,6 +1,9 @@
 #include <libsatsuma/Problems/CostFunction.hh>
 #include <algorithm>
 #include <cassert>
+#ifndef NDEBUG
+#  include <iostream>
+#endif
 
 namespace Satsuma::CostFunction {
 
@@ -8,17 +11,19 @@ BaseObjective::~BaseObjective() = default;
 
 int find_optimal_integer(CostFunction::Function const& _f, int _lower, int _upper)
 {
+    assert(_lower <= _upper);
     using FlowScalar = int;
     using CostScalar = double;
     auto target = CostFunction::get_guess(_f);
     auto floor = static_cast<FlowScalar>(std::llround(std::floor(target)));
     auto ceil = static_cast<FlowScalar>(std::llround(std::ceil(target)));
-    floor = std::max(floor, _lower);
-    ceil = std::min(floor, _upper);
+    floor = std::clamp(floor, _lower, _upper);
+    ceil = std::clamp(ceil, _lower, _upper);
     FlowScalar guess = floor;
     if(floor > ceil) {
-        assert(false);
-        // TODO: emit warning!
+#ifndef NDEBUG
+        std::cerr << "WARNING: floor > ceil" << std::endl;
+#endif
         return floor;
     } else {
         auto min_cost = std::numeric_limits<CostScalar>::infinity();
